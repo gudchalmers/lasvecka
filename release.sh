e4520d4dca7d3fa033665d335f8b46881f000194
Stefan Chan <32509435+Flakt@users.noreply.github.com>
2021-01-20 17:39:13 +0100

    Update release.sh
    Updates should now go towards gudchs repo

--- release.sh
+++ release.sh
@@ -1,9 +1,9 @@
 # Frontend
 docker build --tag lasvecka_frontend:latest -f ./lasvecka-react/prod.Dockerfile ./lasvecka-react
 docker tag lasvecka_frontend:latest flakt/lasvecka_frontend:latest
-docker push flakt/lasvecka_frontend:latest
+docker push gudchs/lasvecka_frontend:latest
 
 # Backend
 docker build --tag lasvecka_backend:latest -f ./lasvecka-python/dev.Dockerfile ./lasvecka-python
 docker tag lasvecka_backend:latest flakt/lasvecka_backend:latest
-docker push flakt/lasvecka_backend:latest
+docker push gudchs/lasvecka_backend:latest
